2737ec9571a8807f4661bedf0f31e2f62d4f59ba
hermannm <hermann.morkrid@gmail.com>
2023-03-14 18:58:48 +0100

    add strntoul string utility function

--- src/utils/strings.c
+++ src/utils/strings.c
@@ -46,3 +46,105 @@ char *strncat(char *const target_string, const char *const source_string, const 
 
     return target_string;
 }
+
+enum strntoul_result
+strntoul(uint64 *const target_int, const char *source_string, const uint max_length, uint base)
+{
+    if (max_length == 0) {
+        return STRNTOUL_ERR_NO_NUMBER_WITHIN_MAX_LENGTH;
+    }
+
+    int digit = *source_string++;
+    uint characters_parsed = 1;
+
+    // Disallow leading - for unsigned integer
+    if (digit == '-') {
+        return STRNTOUL_ERR_NEGATIVE_NUMBER;
+    }
+
+    // Strip leading +
+    if (digit == '+') {
+        if (characters_parsed >= max_length) {
+            return STRNTOUL_ERR_NO_NUMBER_WITHIN_MAX_LENGTH;
+        }
+
+        digit = *source_string++;
+        characters_parsed++;
+    }
+
+    // Strips leading 0x for base 0 or 16
+    if ((base == 0 || base == 16) && digit == '0' && (characters_parsed < max_length) &&
+        (*source_string == 'x' || *source_string == 'X'))
+    {
+        if (characters_parsed + 2 > max_length) {
+            return STRNTOUL_ERR_NO_NUMBER_WITHIN_MAX_LENGTH;
+        }
+
+        base = 16;
+        digit = source_string[1];
+        source_string += 2;
+        characters_parsed += 2;
+    }
+
+    // Deduce base from leading digit if base was not given and was not hexadecimal
+    if (base == 0) {
+        base = digit == '0' ? 8 : 10;
+    }
+
+    const uint64 uint64_max = (uint64)-1;
+
+    // Max value to parse for the given base
+    uint64 cutoff = uint64_max / (uint64)base;
+
+    // If at cutoff: max remainder to allow (since / rounds down for integers)
+    int max_remainder_at_cutoff = uint64_max % (uint64)base;
+
+    uint64 parsed_int = 0;
+    while (digit != '\0' && characters_parsed <= max_length) {
+        // Exploits the fact that chars are integers, to translate characters to digits
+        if (digit >= '0' && digit <= '9') {
+            digit -= '0';
+        } else if (digit >= 'A' && digit <= 'Z') {
+            digit = digit - 'A' + 10;
+        } else if (digit >= 'a' && digit <= 'z') {
+            digit = digit - 'a' + 10;
+        } else {
+            return STRNTOUL_ERR_INVALID_DIGIT;
+        }
+
+        if (digit >= base) {
+            return STRNTOUL_ERR_INVALID_DIGIT;
+        }
+
+        if (parsed_int > cutoff || (parsed_int == cutoff && digit > max_remainder_at_cutoff)) {
+            return STRNTOUL_ERR_OUT_OF_RANGE;
+        }
+
+        parsed_int *= base;
+        parsed_int += digit;
+
+        digit = *source_string++;
+        characters_parsed++;
+    }
+
+    *target_int = parsed_int;
+    return STRNTOUL_SUCCESS;
+}
+
+char *strntoul_error_message(const enum strntoul_result result)
+{
+    switch (result) {
+    case STRNTOUL_SUCCESS:
+        return "Success";
+    case STRNTOUL_ERR_NO_NUMBER_WITHIN_MAX_LENGTH:
+        return "Failed to parse number from string within provided max length";
+    case STRNTOUL_ERR_NEGATIVE_NUMBER:
+        return "Cannot parse negative number to unsigned integer";
+    case STRNTOUL_ERR_INVALID_DIGIT:
+        return "Invalid digit encountered in parsed string";
+    case STRNTOUL_ERR_OUT_OF_RANGE:
+        return "Parsed number exceeded maximum value for unsigned integer";
+    }
+
+    return "Undefined error code";
+}
--- src/utils/strings.h
+++ src/utils/strings.h
@@ -23,4 +23,32 @@ char *strncpy(char *target_string, const char *source_string, uint max_length);
  */
 char *strncat(char *target_string, const char *source_string, uint max_length);
 
+enum strntoul_result {
+    STRNTOUL_SUCCESS = 0,
+    STRNTOUL_ERR_NO_NUMBER_WITHIN_MAX_LENGTH = -1,
+    STRNTOUL_ERR_NEGATIVE_NUMBER = -2,
+    STRNTOUL_ERR_INVALID_DIGIT = -3,
+    STRNTOUL_ERR_OUT_OF_RANGE = -4,
+};
+
+/**
+ * Attempts to parse the given `source_string`, up to `max_length` characters, as a number of the
+ * given `base`, into the unsigned long `target_int`. If base 0 is provided, tries to deduce the
+ * base from the string, defaulting to base 10.
+ *
+ * If parsing failed, returns one of the error codes from `strntoul_result`.
+ *
+ * Adapted from Apple's libc implementation of `strtoul` and `strtol`:
+ * - https://opensource.apple.com/source/Libc/Libc-262/stdlib/strtoul.c.auto.html
+ * - https://opensource.apple.com/source/Libc/Libc-825.25/stdlib/FreeBSD/strtol.c.auto.html
+ */
+enum strntoul_result
+strntoul(uint64 *target_int, const char *source_string, uint max_length, uint base);
+
+/**
+ * Returns an appropriate error message for the given result.
+ * Assumes the result has already been checked as not a success.
+ */
+char *strntoul_error_message(const enum strntoul_result result);
+
 #endif // STRINGS_H
